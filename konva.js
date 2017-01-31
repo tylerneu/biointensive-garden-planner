1f4e54e43c837e9a0b43f642088e59d1f1af7e34
Tyler Neu <tyler.neu@icainformatics.com>
2017-01-30 22:27:29 -0600

    Added form for selecting different spacings and bed sizes. Addressed widths not divisible by a hard-coded arbitrary number.

--- konva.js
+++ konva.js
@@ -1,61 +1,115 @@
 $(document).ready(function() {
 
-  var width = 240;
-  var height = 480;
+  plant();
+
+  $("#form").submit(function(event) {
+    plant()
+    event.preventDefault();
+  });
+
+  $("#plant_spacing").change(function() {
+    plant()
+  });
+
+});
+
+function plant() {
+
+  var width = $("#width").val() * 120;
+  var height = $("#height").val() * 120;
+  var radius = $("#plant_spacing").val() * 10;
+  var hex_height = .5 * Math.sqrt(3) * radius;
 
   var stage = new Konva.Stage({
-    container: 'draw-shapes',   // id of container <div>
+    container: 'draw-shapes',
     width: width,
     height: height
   });
 
-  var radius = 60;
-  var hex_height = .5 * Math.sqrt(3) * radius;
-
-  var layer = new Konva.Layer();
   var plant_layer = new Konva.Layer();
+  var grid_layer = new Konva.Layer();
 
-  var y = (height / 2 * -1);
-  var x_offset = false;
+  grid_layer.add(new Konva.Line({
+    points: [0, 0, height/1.732, height],
+    stroke: 'black',
+    strokeWidth: 1,
+  }));
 
-  while (y < height / 2 + radius) {
+  var y_offset = 0;
+  var x_offset = -width * radius;
 
-    var x = width / 2 * -1;
+  while (x_offset < width * 3) {
 
-    if (x_offset) {
-     x += radius;
-    }
+    grid_layer.add(new Konva.Line({
+      points: [0 + x_offset, 0 + y_offset, height/1.732 + x_offset, height],
+      stroke: 'black',
+      strokeWidth: 1,
+    }));
 
-    while (x < width / 2 + radius) {
+    grid_layer.add(new Konva.Line({
+      points: [0 + x_offset, 0 + y_offset, height/-1.732 + x_offset, height],
+      stroke: 'black',
+      strokeWidth: 1,
+    }));
 
-      addSpacing(stage, layer, radius, x, y);
-
-      addPlant(stage, plant_layer, x + width/2, y + height/2, radius);
-      addPlant(stage, plant_layer, x + width/2 - radius/2, y + height/2 - (hex_height), radius);
-      addPlant(stage, plant_layer, x + width/2 + radius/2, y + height/2 - (hex_height), radius);
-      addPlant(stage, plant_layer, x + width/2 + radius, y + height/2, radius);
-
-      x += radius * 2;
-    }
-
-    y += hex_height * 2;
-    x_offset = !x_offset;
+    x_offset += radius;
   }
 
-  stage.add(layer);
-  stage.add(plant_layer);
+  y_offset = hex_height;
+
+  while (y_offset < height) {
+    grid_layer.add(new Konva.Line({
+      points: [0, 0 + y_offset, width, y_offset],
+      stroke: 'black',
+      strokeWidth: 1,
+    }));
+
+    y_offset += hex_height;
+  }
+
+  // ========================================================
+
+  var y = 0;
+  var x_offset = false;
 
   var plant_count = 0;
-  plant_layer.children.forEach(function(child) {
-    if (child.name() == 'plant') { plant_count++; };
-  })
+
+  while (y < height) {
+
+    var x = 0;
+    if (x_offset) {
+      x += radius/2;
+    }
+
+    while (x < width) {
+
+      if (addPlant(stage, plant_layer, x, y, radius)) {
+        plant_count ++;
+      }
+      x += radius;
+    }
+
+    y += hex_height;
+    x_offset = !x_offset;
+
+  }
+
+  // ========================================================
+
   $('#plant_count').html(plant_count);
 
-});
+  stage.add(grid_layer);
+  stage.add(plant_layer);
+}
 
 function addPlant(stage, layer, x, y, radius) {
 
-  if (x - radius/2 + 1 > 0 && x + radius/2 + 1 < stage.getWidth() && y - radius/2 + 1 > 0 & y + radius/2 + 1 < stage.getHeight()) {
+  if (x - radius/2 + 1 > 0 &&
+      x + radius/2 - 1 < stage.getWidth() &&
+      y - radius/2 + 1 > 0 &&
+      y + radius/2 - 1 < stage.getHeight()
+  ) {
+
     layer.add(new Konva.Circle({
       x: x,
       y: y,
@@ -68,76 +122,80 @@ function addPlant(stage, layer, x, y, radius) {
     layer.add(new Konva.Circle({
       x: x,
       y: y,
-      radius: 10,
+      radius: radius/4,
       fill: 'green',
       strokeWidth: 0,
       name: 'plant',
     }));
+
+    return true;
   }
 
+  return false;
+
 }
 
-function addSpacing(stage, layer, radius, x, y) {
-
-  var group = new Konva.Group({
-    x: x,
-    y: y,
-   });
-
-   var points = [
-     stage.getWidth() / 2 - radius, stage.getHeight() / 2,
-     stage.getWidth() / 2 + radius, stage.getHeight() / 2
-   ];
-
-  group.add(new Konva.Line({
-    points: points,
-    stroke: '#ACACAC',
-    strokeWidth: 1,
-  }));
-
-
-  points = [
-    stage.getWidth() / 2 - radius / 2,
-    stage.getHeight() / 2 - radius + 10 + 6,
-
-    stage.getWidth() / 2 + radius / 2,
-    stage.getHeight() / 2 + radius - 10 - 6,
-  ];
-
-  group.add(new Konva.Line({
-    points: points,
-    stroke: '#ACACAC',
-    strokeWidth: 1,
-  }));
-
-
-  points = [
-    stage.getWidth() / 2 - radius / 2,
-    stage.getHeight() / 2 + radius - 10 - 6,
-
-    stage.getWidth() / 2 + radius / 2,
-    stage.getHeight() / 2 - radius + 10 + 6
-  ];
-
-  group.add(new Konva.Line({
-    points: points,
-    stroke: '#ACACAC',
-    strokeWidth: 1,
-  }));
-
-
-  var plant = new Konva.RegularPolygon({
-    x: stage.getWidth() / 2,
-    y: stage.getHeight() / 2,
-    radius: radius,
-    sides: 6,
-    stroke: '#ACACAC',
-    strokeWidth: 0,
-    rotation: 30,
-  });
-
-
-  group.add(plant);
-
-  layer.add(group);
-}
+// function addSpacing(stage, layer, radius, x, y) {
+//
+//   var group = new Konva.Group({
+//     x: x,
+//     y: y,
+//    });
+//
+//    var points = [
+//      stage.getWidth() / 2 - radius, stage.getHeight() / 2,
+//      stage.getWidth() / 2 + radius, stage.getHeight() / 2
+//    ];
+//
+//   group.add(new Konva.Line({
+//     points: points,
+//     stroke: '#ACACAC',
+//     strokeWidth: 1,
+//   }));
+//
+//
+//   points = [
+//     stage.getWidth() / 2 - radius / 2,
+//     stage.getHeight() / 2 - radius + 10 + 6,
+//
+//     stage.getWidth() / 2 + radius / 2,
+//     stage.getHeight() / 2 + radius - 10 - 6,
+//   ];
+//
+//   group.add(new Konva.Line({
+//     points: points,
+//     stroke: '#ACACAC',
+//     strokeWidth: 1,
+//   }));
+//
+//
+//   points = [
+//     stage.getWidth() / 2 - radius / 2,
+//     stage.getHeight() / 2 + radius - 10 - 6,
+//
+//     stage.getWidth() / 2 + radius / 2,
+//     stage.getHeight() / 2 - radius + 10 + 6
+//   ];
+//
+//   group.add(new Konva.Line({
+//     points: points,
+//     stroke: '#ACACAC',
+//     strokeWidth: 1,
+//   }));
+//
+//
+//   var plant = new Konva.RegularPolygon({
+//     x: stage.getWidth() / 2,
+//     y: stage.getHeight() / 2,
+//     radius: radius,
+//     sides: 6,
+//     stroke: '#ACACAC',
+//     strokeWidth: 0,
+//     rotation: 30,
+//   });
+//
+//
+//   group.add(plant);
+//
+//   layer.add(group);
+// }
